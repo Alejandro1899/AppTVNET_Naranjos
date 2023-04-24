ee38d747f3a06743907efe351ba984d41872a030
Alejandro1899 <ale.arteaga99@outlook.com>
2023-04-23 20:43:15 -0600

    Agregar archivos de proyecto.

---
+++ AppTVNET_Naranjos/Vistas/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppTVNET_Naranjos
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
