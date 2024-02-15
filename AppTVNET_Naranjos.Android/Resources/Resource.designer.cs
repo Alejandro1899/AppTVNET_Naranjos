cb3b3dcd1b085e2a003ed12b476d0aa141040fec
Alejandro1899 <ale.arteaga99@outlook.com>
2024-02-15 12:42:33 -0600

    rama nueva

--- AppTVNET_Naranjos.Android/Resources/Resource.designer.cs
+++ AppTVNET_Naranjos.Android/Resources/Resource.designer.cs
@@ -14,7 +14,7 @@ namespace AppTVNET_Naranjos.Droid
 {
 	
 	
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "13.2.0.99")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "13.2.2.120")]
 	public partial class Resource
 	{
 		
