98374b6436a6b7344aa1859a4aab2336f686859b
Alejandro1899 <ale.arteaga99@outlook.com>
2024-03-09 01:39:10 -0600

    INICO Y FUNCION SCROLL VIEW

---
+++ AppTVNET_Naranjos/Datos/ImgsScrolled.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppTVNET_Naranjos.Datos
+{
+    public class ImgsScrolled
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
--- AppTVNET_Naranjos/Vistas/VHomePage.xaml.cs
+++ AppTVNET_Naranjos/Vistas/VHomePage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using AppTVNET_Naranjos.Datos;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,19 @@ namespace AppTVNET_Naranjos.Vistas
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class VHomePage : ContentPage
     {
+        public List<ImgsScrolled> Imagenes {  get; set; }
         public VHomePage()
         {
             InitializeComponent();
+
+            Imagenes = new List<ImgsScrolled>
+            {
+                new ImgsScrolled { Name= "Fibra Optica",Url ="https://i.ibb.co/x59jdzK/fibra-optica.jpg"},
+                new ImgsScrolled { Name= "Paqutes ",Url ="https://i.ibb.co/WvLqTn0/planes-paquetes.jpg"},
+                new ImgsScrolled { Name= "Conocenos",Url ="https://i.ibb.co/Ch2Wyv8/publicidad.jpg"},
+                new ImgsScrolled { Name= "Ubicación",Url ="https://i.ibb.co/bvJZmRJ/ubicacion.jpg"}
+            };
+            this.BindingContext = this;
         }
     }
 }
